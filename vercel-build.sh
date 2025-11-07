54a315d1c6459645bb58a7cc7fad9c33f3337763
Miguel Angel Jimenez Ramirez <jimenez.rma5@gmail.com>
2025-11-07 13:37:40 -0600

    Update build output directory in vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,5 +3,4 @@ echo "🚀 Iniciando build de WhiskerTales by Fatipandi..."
 cd client
 npm install
 npm run build
-echo "✅ Build finalizado. Archivos generados en /client/dist/public"
-
+echo "✅ Build finalizado. Archivos generados en /client/dist"
